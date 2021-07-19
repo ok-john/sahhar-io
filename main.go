49d0c1db930b70c8eb5e17609a6ea6bf7179ce09
reflect <jfs>
2021-07-19 11:41:16 -0400

    refactor-main

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"crypto/tls"
-	"flag"
 	"log"
 	"net/http"
 	"path"
@@ -19,94 +18,28 @@ import (
 )
 
 const (
-	CERTS_PATH                = "/etc/letsencrypt/live/sahhar.io"
-	CERT_FN                   = CERTS_PATH + "/fullchain.pem"
-	KEY_FN                    = CERTS_PATH + "/privkey.pem"
 	PUB_URL            string = "sahhar.io"
+	CERTS_PATH         string = "/etc/letsencrypt/live/" + PUB_URL
+	CERT_FN            string = CERTS_PATH + "/fullchain.pem"
+	KEY_FN             string = CERTS_PATH + "/privkey.pem"
 	DAEMON_ENVIRONMENT string = "/var/lib/local/sahhar-io"
 )
 
 var (
-	MAX_TIMEOUT = time.Hour * 2
-	user_env    = DAEMON_ENVIRONMENT
+	MAX_TIMEOUT             = time.Hour * 2
+	l           *log.Logger = log.Default()
 )
 
 func main() {
-	// Start
+	// defer resolution of ctx, ensures no detached servers persist the daemon
 	ctx := context.Background()
 	defer ctx.Done()
-	environment := flag.String("e", DAEMON_ENVIRONMENT, "env path for daemon")
-	flag.Parse()
-	user_env = *environment
+
+	// concurrently launch redirect servers
 	go redirect(ctx)
-	log.Fatal(launch())
-}
 
-func inEnv(relativePath string) string {
-	return path.Join(user_env, relativePath)
-}
-
-func launch() error {
-	l := log.Default()
-	app := fiber.New()
-	app.Static("/", inEnv("public"), fiber.Static{Browse: true})
-	app.Use("/",
-		func(c *fiber.Ctx) error {
-			requestDump := strings.Join(strings.Split(c.Request().String(), "\n"), "\n\t\t")
-			fullUrl := c.Request().URI().String()
-			timestamp := time.Now().Format(time.Stamp)
-			requestIPs := append(c.IPs(), c.IP())
-			l.Printf("| %s | URL=%s | IP=%s || Request\n\t\t%+v", timestamp, fullUrl, requestIPs, requestDump)
-			return c.Next()
-		},
-		etag.New(),
-		requestid.New(),
-		favicon.New(favicon.Config{
-			File: inEnv("static/img/favicon.ico"),
-		}),
-		func(c *fiber.Ctx) error {
-			c.Response().Header.Add("Strict-Transport-Security", "max-age=63072000")
-			c.Response().Header.Add("X-Content-Type-Options", "nosniff")
-			return c.Next()
-		},
-		limiter.New(limiter.Config{
-			Next: func(c *fiber.Ctx) bool {
-				if c.IP() == "127.0.0.1" {
-					return true
-				}
-				splitIp := strings.Split(c.IP(), ".")
-				if len(splitIp) >= 4 && splitIp[0] == "10" && splitIp[1] == "10" && splitIp[2] == "10" {
-					if i, err := strconv.Atoi(splitIp[3]); err == nil && (i >= 12 && i <= 32) {
-						return true
-					}
-				}
-				return false
-			},
-			Max:        20,
-			Expiration: 30 * time.Second,
-			LimitReached: func(c *fiber.Ctx) error {
-				return c.Send([]byte("Take a breather, bud."))
-			},
-		}))
-
-	//Run
-	if err := LaunchTLS(app); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// fullAddr should be format
-//      ip:port
-func __new_redirect(fullAddr string) *http.Server {
-	return &http.Server{
-		Addr: fullAddr,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-			http.Redirect(w, req, "https://"+PUB_URL+req.RequestURI, http.StatusMovedPermanently)
-		}),
-		ReadTimeout: 60 * time.Second, WriteTimeout: 60 * time.Second,
-	}
+	// launch main server
+	log.Fatal(launch(ctx))
 }
 
 func redirect(ctx context.Context) error {
@@ -148,4 +81,90 @@ func LaunchTLS(app *fiber.App) error {
 		return err
 	}
 	return nil
+}
+
+func inEnv(relativePath string) string {
+	return path.Join(DAEMON_ENVIRONMENT, relativePath)
+}
+
+func launch(ctx context.Context) error {
+
+	// fiber
+	app := fiber.New()
+
+	// Static fs
+	app.Static("/", inEnv("public"), fiber.Static{Browse: true})
+
+	// Middleware
+	app.Use("/",
+		logRequests,
+		addHeaders,
+		newLimiter,
+		faviconHandler(),
+		etag.New(),
+		requestid.New(),
+	)
+
+	// Serve TLS
+	if err := LaunchTLS(app); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func faviconHandler() fiber.Handler {
+	return favicon.New(favicon.Config{
+		File: inEnv("static/img/favicon.ico"),
+	})
+}
+
+func newLimiter() fiber.Handler {
+	return limiter.New(limiter.Config{
+		Next:         limiterMiddleware,
+		Max:          20,
+		Expiration:   30 * time.Second,
+		LimitReached: limiterHandler,
+	})
+}
+
+func limiterHandler(c *fiber.Ctx) error {
+	return c.Send([]byte("Take a breather, bud."))
+}
+
+func limiterMiddleware(c *fiber.Ctx) bool {
+	splitIp := strings.Split(c.IP(), ".")
+	if len(splitIp) >= 4 && splitIp[0] == "10" && splitIp[1] == "10" && splitIp[2] == "10" {
+		if i, err := strconv.Atoi(splitIp[3]); err == nil && (i >= 12 && i <= 32) {
+			return true
+		}
+	}
+	return false
+}
+
+func addHeaders(c *fiber.Ctx) error {
+	c.Response().Header.Add("Strict-Transport-Security", "max-age=63072000")
+	c.Response().Header.Add("X-Content-Type-Options", "nosniff")
+	return c.Next()
+}
+
+func logRequests(c *fiber.Ctx) error {
+	requestDump := strings.Join(strings.Split(c.Request().String(), "\n"), "\n\t\t")
+	fullUrl := c.Request().URI().String()
+	timestamp := time.Now().Format(time.Stamp)
+	requestIPs := append(c.IPs(), c.IP())
+	l.Printf("| %s | URL=%s | IP=%s || Request\n\t\t%+v", timestamp, fullUrl, requestIPs, requestDump)
+	return c.Next()
+}
+
+// fullAddr should be format
+//      ip:port
+func __new_redirect(fullAddr string) *http.Server {
+	return &http.Server{
+		Addr: fullAddr,
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			http.Redirect(w, req, "https://"+PUB_URL+req.RequestURI, http.StatusMovedPermanently)
+		}),
+		ReadTimeout: 60 * time.Second, WriteTimeout: 60 * time.Second,
+	}
 }
